8f6e34af6f295379fb7185b41ff5a7729ec87e25
heyao <heyao@haimaqingfan.com>
2018-02-04 14:35:31 +0800

    htf_spider新增utils

---
+++ heretofore/htfspider/htfspider/utils.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+create on 2018-02-04 下午2:33
+
+author @heyao
+"""
+
+import os
+import requests
+
+usernm = os.environ.get("SPIDER_USERNAME")
+passwd = os.environ.get("SPIDER_PASSWORD")
+
+
+def authorized_requests(method, url, username='', password='', **kwargs):
+    username = username or usernm
+    password = password or passwd
+    headers = kwargs.pop('headers', {})
+    headers.update({'User-Agent': 'htf-slave'})
+    response = requests.request(method, url, headers=headers, auth=(username, password), **kwargs)
+    content = response.content
+    response.close()
+    return content
